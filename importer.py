820dffd0a2ec570319cc1564e07c171b5a134374
Burhan  Can Akkuş <burgcanmail@gmail.com>
2021-07-02 12:20:32 +0300

    Added Reports

--- importer.py
+++ importer.py
@@ -5,3 +5,8 @@ print("Original Text:")
 print("Hansel and Gretel are trapped in the witch's house. They have to find a way out before she catches them!")
 print("Text after Coreference Resolution")
 print("Hansel and Gretel are trapped in the witch's house. Hansel and Gretel have to find a way out before the witch catches Hansel and Gretel!")
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+sia = SentimentIntensityAnalyzer()
+print(sia.polarity_scores("Poor Granny did not have time to say a word, before the wolf gobbled her up!​"))
